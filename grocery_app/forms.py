43aea15844c8289ae70ee55c769bd8cdd027bd9f
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-02-02 18:55:07 -0800

    complete forms

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -5,21 +5,15 @@ from wtforms.validators import DataRequired, Length, URL
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
-
-    # TODO: Add the following fields to the form class:
-    # - title - StringField
-    # - address - StringField
-    # - submit button
-    pass
+    title = StringField('Store Title', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
-
-    # TODO: Add the following fields to the form class:
-    # - name - StringField
-    # - price - FloatField
-    # - category - SelectField (specify the 'choices' param)
-    # - photo_url - StringField
-    # - store - QuerySelectField (specify the `query_factory` param)
-    # - submit button
-    pass
+    name = StringField('Name', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    category = SelectField('Category', choices=ItemCategory.choices())
+    photo_url = StringField('Photo', validators=[DataRequired()])
+    store = QuerySelectField('Stores', query_factory=lambda: GroceryStore.query)
+    submit = SubmitField('Submit')
