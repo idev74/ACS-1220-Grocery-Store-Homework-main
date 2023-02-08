a7d8cd3a90a74b4fe69a0e0ff8dcefbca041a851
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-02-08 15:29:43 -0800

    progress

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from grocery_app.config import Config
 import os
@@ -10,15 +9,18 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
-# flask_login.LoginManager
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
 from .models import User
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-bcrypt = Bcrypt(app)
+from grocery_app.routes import main
+
+app.register_blueprint(main)
+
+with app.app_context:
+    db.create_all()
--- grocery_app/models.py
+++ grocery_app/models.py
@@ -18,6 +18,8 @@ class GroceryStore(db.Model):
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
 
 class GroceryItem(db.Model):
     """Grocery Item model."""
@@ -27,5 +29,15 @@ class GroceryItem(db.Model):
     category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
     photo_url = db.Column(URLType)
     store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
+    db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
+
+class User(db.Model):
+    """User Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return f'<User: {self.username}>'
